94a0b45bac1b2d85af40714c0f9d182106035e35
Nawal-Lotfi <n.alab@yahoo.fr>
2020-08-19 15:51:00 +0200

    refacto

---
+++ client/src/components/HomePage/HomePage/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// Styles
+import './styles.scss';
+
+const HomePage = () => (
+  <div className="explications">
+    <div className="explications-box">
+      Step 1
+    </div>
+    <div className="explications-box">
+      Step 2
+    </div>
+    <div className="explications-box">
+      Step 3
+    </div>
+    <button type="submit" className="main-button">
+      En route!
+    </button>
+  </div>
+
+);
+
+export default HomePage;
