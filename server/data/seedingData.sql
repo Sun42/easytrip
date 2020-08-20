1c72eea3b8893225929000e9f0ce598f658df511
Sarayachak <sarahchloe.chakra@gmail.com>
2020-08-20 11:12:10 +0200

    modified seeding data according to database tables modifications

--- server/data/seedingData.sql
+++ server/data/seedingData.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (email, password, nom, prenom)
+INSERT INTO user (email, password, name, surname)
 ('test@ŧest.com', 'test', 'Test', 'Test')
 ('thrall@orgrimmar.com', 'test','Durotar', 'Thrall'),
 ('garrosh@hurlenfer.com','test', 'Hurlenfer', 'Garrosh'),
@@ -6,25 +6,25 @@ INSERT INTO user (email, password, nom, prenom)
 ('edwin@vancleef.com', 'test', 'Vancleef', 'Edwin'),
 ('malfurion@hurlorage.com', 'test', 'Hurlorage', 'Malfurion');
 
-INSERT INTO carnet(nom, ville, date_depart, date_retour, user_id) VALUES
+INSERT INTO travelogue(name, city, date_departure, date_return, user_id) VALUES
 ('Roadtrip dans le sud', 'Narbonne', '2016-06-22', '2016-07-01', 1),
 ('Tourisme à Paris', 'Paris', '2017-05-22', '2016-05-30', 2),
 ('Découverte de la gastronomie Lyonnaise', 'Lyon', '2017-05-22', '2016-05-30', 3);
 
-INSERT INTO categorie(nom) VALUES
+INSERT INTO category(label) VALUES
 ('Gastronomie'), 
 ('Randonnée'), 
 ('Activité aquatique')
 ('Promenade')
 ('Bar');
 
-INSERT INTO activites(nom, information, dispo, localisation, carnet_id, categorie_id) VALUES
-('Auberge du Lion', 'Haute cuisine et grande carte de vins', '18:00:00', 'lat : 45, lon: 32', 2, 1),
-('Aquaparc', 'Piscines et jeux aquatiques pour tous les âges', '09:00:00', 'lat : 95, lon: 12', 1, 3),
-('Falaise du malaise', 'Enorme circuit de randonnée pour les plus aguerris', '07:00:00', 'lat : 15, lon: 32', 1, 2);
+INSERT INTO activity(name, information, localisation, travelogue_id, category_id) VALUES
+('Auberge du Lion', 'Haute cuisine et grande carte de vins', 'lat : 45, lon: 32', 2, 1),
+('Aquaparc', 'Piscines et jeux aquatiques pour tous les âges','lat : 95, lon: 12', 1, 3),
+('Falaise du malaise', 'Enorme circuit de randonnée pour les plus aguerris','lat : 15, lon: 32', 1, 2);
 
 
--- Il faudra rajouter des contraintes sur activites.dispo pour qu'une telle heure de la journée = matin, aprem ou soir 
+
 -- Il faudra faire une migration sur carnet.user_id où l'on va transformer le user_id en not null 
 
 
