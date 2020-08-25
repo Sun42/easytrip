7e8dccdf25a0e410e764c994689ef6ee6945b6b1
Sarayachak <sarahchloe.chakra@gmail.com>
2020-08-25 14:28:25 +0200

    more cleaning

--- server/app/middlewares/userMid.js
+++
@@ -1,8 +0,0 @@
-// Ce module sert de parser
-const userMiddleware = (request, response, next) => {
-    
-    response.locals.user = request.session.user;
-    next();
-};
-
-module.exports = userMiddleware;
