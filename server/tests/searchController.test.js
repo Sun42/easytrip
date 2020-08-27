5625b8247e466b0aa6497a7420ce86c597db1d6f
Christophe Sundas <sundas.christophe@gmail.com>
2020-08-27 18:37:38 +0200

    fix test

--- server/tests/searchController.test.js
+++ server/tests/searchController.test.js
@@ -70,7 +70,7 @@ describe('test overpass url generation', () => {
         const cp = {south : "45.7073666", north: "45.8082628",
         west : "4.7718134", east: "4.8983774"};
         const filters = ['pub'];
-        const expected_response = "http://overpass-api.de/api/interpreter?data=[out:json][timeout:25][bbox:45.7073666,4.7718134,45.8082628,4.8983774];node[amenity=pub];out;node[amenity=biergarten];out;node[amenity=bar];out;node[amenity=restaurant];out;node[amenity=fast_food];out;node[amenity=food_court];out;node[amenity=cafe];out;";
+        const expected_response = "http://overpass-api.de/api/interpreter?data=[out:json][timeout:25][bbox:45.7073666,4.7718134,45.8082628,4.8983774];node[amenity=pub];out;node[amenity=biergarten];out;node[amenity=bar];out;";
         const response = sc.overpassURL(cp, filters);
         expect(response).toBe(expected_response);
     });
