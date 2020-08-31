679993a4d8fe9580b1cbc224fc6fb24326575aa6
Sarayachak <sarahchloe.chakra@gmail.com>
2020-08-31 15:39:46 +0200

    corrected client side ok

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 // Styles
 import './styles.scss';
+
 const Header = ({
   isLogged,
   loggedMessage,
@@ -32,61 +33,9 @@ const Header = ({
           <NavLink activeClassName="active" to="/sign-up">Inscription</NavLink>
         </div>
       )}
-
-
-// router
-import { NavLink } from 'react-router-dom';
-
-// semantic-ui
-import { Button, Icon } from 'semantic-ui-react';
-
-// Styles
-import './styles.scss';
-import Logo from '../../assets/newLogo.PNG';
-
-// composants
-import ModalAddTrip from '../../containers/ModalAddTrip';
-
-const Header = () => (
-  <div className="header">
-    <img className="logo" src={Logo} alt="name of the page" />
-    <div className="login">
-      <ModalAddTrip />
-      <NavLink to="/login">
-        <Button
-          circular
-          icon="suitcase"
-          color="orange"
-          size="large"
-        />
-      </NavLink>
-      <NavLink to="/login">
-        <Button animated>
-          <Button.Content visible>Connexion</Button.Content>
-          <Button.Content hidden>
-            <Icon name="arrow right" />
-          </Button.Content>
-        </Button>
-      </NavLink>
-      <NavLink to="/login">
-        <Button
-          circular
-          icon="user"
-          color="black"
-          size="large"
-        />
-      </NavLink>
-      <NavLink to="/inscription">
-        <Button animated>
-          <Button.Content visible>Inscription</Button.Content>
-          <Button.Content hidden>
-            <Icon name="arrow right" />
-          </Button.Content>
-        </Button>
-      </NavLink>
-
     </div>
   </div>
+
 );
 Header.propTypes = {
   isLogged: PropTypes.bool,
