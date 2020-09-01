e9f271924554d6df18621f30e09732e4447853ba
Christophe Sundas <sundas.christophe@gmail.com>
2020-09-01 13:18:09 +0200

    close db connexions after tests

--- server/tests/route.search.test.js
+++ server/tests/route.search.test.js
@@ -7,12 +7,12 @@ const { available_filters } = require('../app/helpers/filterHelper');
 
 describe('/api/search ', () => {
     const mock = new MockAdapter(axios);
-    /*
-    beforeEach(() => {
-        });
-    */
+
     afterEach(() => {
         mock.reset();
+    });
+    afterAll(() => {
+        app.close();
     });
 
     test('/api/search no location in query', async () => {
@@ -22,7 +22,7 @@ describe('/api/search ', () => {
         expect(response.body.error).toMatch(/missing location/);
     });
 
-    test('/api/search/ empty location', async () => {
+    test('/api/search/ empty location parameter', async () => {
         const response = await request(app).get('/api/search?location=');
         expect(response.statusCode).toBe(400);
         expect(response.body).toHaveProperty('error');
@@ -59,5 +59,43 @@ describe('/api/search ', () => {
             expect(response.body.elements[key].length).toBeGreaterThan(0);
         }
     });
-    // todo mock nomintaim erros response and tests L50-51
+
+    test('/api/search?location=selkrfjelskjlkj error 404:location not found', async () => {
+        mock.onGet(/nominatim.openstreetmap.org/).reply(404, '"not found"');
+        const response = await request(app).get('/api/search?location=selkrfjelskjlkj');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toHaveProperty('isAxiosError');
+        expect(response.body.error.isAxiosError).toBe(true);
+        expect(response.body.error).toHaveProperty('response');
+        expect(response.body.error.response).toHaveProperty('status');
+        expect(response.body.error.response.status).toBe(404);
+        expect(response.body.error.response).toHaveProperty('data');
+        expect(response.body.error.response.data).toBe('not found');
+        expect(response.body.error.response.config).toHaveProperty('url');
+        expect(response.body.error.response.config.url).toBe('https://nominatim.openstreetmap.org/search?q=selkrfjelskjlkj&format=json&addressdetails=1&limit=1');
+    });
+
+    test('/api/search?location=lyon error 503:nominatim unavailable', async () => {
+        mock.onGet(/nominatim.openstreetmap.org/).reply(503, '"nominatim down"');
+        const response = await request(app).get('/api/search?location=lyon');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toHaveProperty('isAxiosError');
+        expect(response.body.error.isAxiosError).toBe(true);
+        expect(response.body.error.response.status).toBe(503);
+        expect(response.body.error.response.data).toBe('nominatim down');
+        expect(response.body.error.response.config.url).toBe('https://nominatim.openstreetmap.org/search?q=lyon&format=json&addressdetails=1&limit=1');
+    });
+    test('/api/search?location=lyon&filters=1 error 503:overpass unavailable', async () => {
+        const mocked_location = require('./nominatim_lyon_location.json');
+        mock.onGet(/nominatim.openstreetmap.org/).reply(200, mocked_location);
+        mock.onGet(/overpass-api.de/).reply(503, '"overpass down"');
+        const response = await request(app).get('/api/search?location=lyon&filters=1');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.error.isAxiosError).toBe(true);
+        expect(response.body.error.response.data).toBe('overpass down');
+        expect(response.body.error.response.status).toBe(503);
+        expect(response.body.error.response.config.url).toMatch('http://overpass-api.de/api/');
+    });
 });
