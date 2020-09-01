38334d79e7d92ebfad14c1a0f538c4263df738ba
Christophe Sundas <sundas.christophe@gmail.com>
2020-09-01 07:33:19 +0200

    lint

--- server/app/middlewares/userMiddleware.js
+++ server/app/middlewares/userMiddleware.js
@@ -2,9 +2,10 @@ const userMiddleware = (request, response, next) => {
     if (request.session.user) {
         console.log('Utilisateur connecté', request.session.user);
         response.locals.user = request.session.user;
-    } else {
+    }
+    else {
         response.locals.user = false;
-        console.log('Utilisateur non connecté', request.session.user)
+        console.log('Utilisateur non connecté', request.session.user);
     }
 
     next();
