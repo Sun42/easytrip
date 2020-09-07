01d0584564d0366155b0001b7f5935f3c9d8a542
Nawal-Lotfi <n.alab@yahoo.fr>
2020-09-07 13:26:17 +0200

    redirection signUp

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+//import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { useState} from "react";
+import { Link} from 'react-router-dom';
+
+import {
+
+  Checkbox,
+
+  Form,
+  
+} from "semantic-ui-react";
 
 import Field from './Field.js/index.js';
 // import { useField } from './hooks';
@@ -19,6 +28,8 @@ const SignUpForm = ({ // modif Sarah
 }) => {
   console.log(SignUpForm);
 
+  const [terms, setTerms] = useState(false);
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
     handleSignUp();
@@ -30,7 +41,7 @@ const SignUpForm = ({ // modif Sarah
         <img src="https://www.bnc.ca/content/dam/fragment/images/Fotolia-79030524-Subscription-Monthly-M-1-e1481143505131.jpg" alt="" />
       </div>
       <div className="login-form">
-      {!isLogged && (
+        {!isLogged && (
         <form method="post" autoComplete="off" className="login-form-element" onSubmit={handleSubmit}>
           <h2>Inscrivez-vous</h2>
 
@@ -67,13 +78,30 @@ const SignUpForm = ({ // modif Sarah
             placeholder=""
             value={password}
           />
+
+<Form.Field
+                        control={Checkbox}
+                        onChange={() => setTerms(!terms)}
+                        id="terms"
+                        label={{
+                            children: [
+                                "J'accepte les",
+                                <a className="internal-link" href="/test">
+                                    termes
+                                </a>,
+                            ],
+                        }}
+                    />
+           
           <button
             type="submit"
             className="login-form-button"
             onClick="submit"
           >
-            Valider mon incription
+          <Link activeClassName="active" to="/result">
+            Valider mon incription </Link>
           </button>
+         
           <Link activeClassName="active" to="/login">
             <span>Vous avez deja un compte ?</span>
           </Link>
